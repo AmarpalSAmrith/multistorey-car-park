43a567e6bc3ad5b65160c0ef2e236e7622431b19
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-04-25 21:39:43 +0100

    Third set of tests

--- src/main/java/com/softwire/training/parking/ParkingSpace.java
+++ src/main/java/com/softwire/training/parking/ParkingSpace.java
@@ -28,4 +28,22 @@ public class ParkingSpace {
         return vehicle.getHeight() <= maxHeight && vehicle.getWidth() <= maxWidth && vehicle.getType() == typeRestriction;
 
     }
+
+    public void parkVehicle(Vehicle vehicle) throws IllegalArgumentException, IllegalStateException {
+        // TODO - replace this!
+    }
+
+    public void vacate() {
+        // TODO - replace this!
+    }
+
+    public Vehicle getParkedVehicle() {
+        // TODO - replace this!
+        return null;
+    }
+
+    public boolean isOccupied() {
+        // TODO - replace this!
+        return false;
+    }
 }
